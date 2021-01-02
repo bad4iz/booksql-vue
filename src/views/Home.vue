c52c48eb3d22f339f91d2af616eb28fa3427276e
bad4iz <develop@bad4iz.ru>
2021-01-02 23:42:07 +0400

    part 3

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,118 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    {{books}}
+    <ApolloQuery :query="categoriesQuery">
+      <template slot-scope="{ result: { data, loading }, isLoading }">
+        <div v-if="isLoading">Loading...</div>
+        <div v-else>
+          <a href="#" class="link-margin" @click.prevent="selectCategory('all')">All</a>
+          <a href="#" class="link-margin" @click.prevent="selectCategory('featured')">Featured</a>
+          <a href="#" v-for="category of data.categories" :key="category.id" class="link-margin"
+             @click.prevent="selectCategory(category)">
+            {{ category.id }}. {{ category.name }}
+          </a>
+        </div>
+      </template>
+    </ApolloQuery>
+
+    <div v-if="selectedCategory === 'all'">
+      <ApolloQuery :query="query">
+        <template slot-scope="{ result: { data, loading }, isLoading }">
+          <div v-if="isLoading">Loading...</div>
+          <div v-else>
+            <div v-for="book of data.books" :key="book.id">
+              <router-link :to="`/book/${book.id}`">
+                {{ book.id }}. {{ book.title }}
+              </router-link>
+              <div>{{book.author}}</div>
+              <img :src="book.image" :alt="book.title">
+            </div>
+          </div>
+        </template>
+      </ApolloQuery>
+    </div>
+
+    <div v-else-if="selectedCategory === 'featured'">
+      <ApolloQuery :query="query" :variables="{ featured: true }">
+        <template slot-scope="{ result: { data, loading }, isLoading }">
+          <div v-if="isLoading">Loading...</div>
+          <div v-else>
+            <div v-for="book of data.booksByFeatured" :key="book.id">
+              <router-link :to="`/book/${book.id}`">
+                {{ book.id }}. {{ book.title }}
+              </router-link>
+              <div>{{book.author}}</div>
+              <img :src="book.image" :alt="book.title">
+            </div>
+          </div>
+        </template>
+      </ApolloQuery>
+    </div>
+    <div v-else>
+      <ApolloQuery :query="query" :variables="{id: selectedCategory}">
+        <template slot-scope="{ result: { data, loading }, isLoading }">
+          <div v-if="isLoading">Loading...</div>
+          <div v-else>
+            <div v-for="book of data.category.books" :key="book.id">
+              <router-link :to="`/book/${book.id}`">
+                {{ book.id }}. {{ book.title }}
+              </router-link>
+              <div>{{book.author}}</div>
+              <img :src="book.image" :alt="book.title">
+            </div>
+          </div>
+        </template>
+      </ApolloQuery>
+    </div>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-import gql from 'graphql-tag'
+import categoryQuery from '@/graphql/queries/Category.gql'
+import categoriesQuery from '@/graphql/queries/Categories.gql'
+import booksQuery from '@/graphql/queries/Books.gql'
+import booksFeaturedQuery from '@/graphql/queries/BooksFeatured.gql'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      categoryQuery,
+      categoriesQuery,
+      booksFeaturedQuery,
+      selectedCategory: 'all',
+      books: [],
+      query: booksQuery,
+    }
   },
-  apollo: {
-    // Simple query that will update the 'hello' vue property
-    books: gql`{books {
-        id
-        name
-    }}`,
+  components: {},
+  methods: {
+    selectCategory(category) {
+      switch (category) {
+        case 'all': {
+          this.query = booksQuery
+          this.selectedCategory = category
+          break
+        }
+        case 'featured': {
+          this.selectedCategory = category
+          this.query = booksFeaturedQuery
+          break;
+        }
+        default: {
+          this.query = categoryQuery
+          this.selectedCategory = category.id
+
+        }
+
+      }
+    }
   },
 }
 </script>
+
+<style>
+.link-margin {
+  margin: 24px;
+}
+</style>

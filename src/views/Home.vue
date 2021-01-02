0fa8862473dbd042e8bac6bd5b87921faec25968
bad4iz <develop@bad4iz.ru>
2021-01-02 21:30:01 +0400

    install apollo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,26 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{books}}
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import gql from 'graphql-tag'
 
 export default {
   name: 'Home',
   components: {
     HelloWorld
-  }
+  },
+  apollo: {
+    // Simple query that will update the 'hello' vue property
+    books: gql`{books {
+        id
+        name
+    }}`,
+  },
 }
 </script>

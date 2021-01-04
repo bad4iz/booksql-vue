b14d62118abf23ea2f09d8a0505a67070185d9ea
bad4iz <develop@bad4iz.ru>
2021-01-05 00:32:36 +0400

    part 4

--- src/router/index.js
+++ src/router/index.js
@@ -19,13 +19,29 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
+    path: '/book/add',
+    name: 'AddBook',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AddBook.vue')
+  },
+  {
     path: '/book/:id',
     name: 'Book',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Book.vue')
-  }
+  },
+  {
+    path: '/book/:id/edit',
+    name: 'EditBook',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/EditBook.vue')
+  },
 ]
 
 const router = new VueRouter({
--- src/views/Book.vue
+++ src/views/Book.vue
@@ -4,19 +4,23 @@
       <template slot-scope="{ result: { data: {book}, loading }, isLoading }">
         <div v-if="isLoading">Loading...</div>
         <div v-else>
-          {{ book }}
           <h1>{{ book.title }}</h1>
           <img :src="book.image" :alt="book.title">
           <div>{{ book.author }}</div>
           <div>{{ book.description }}</div>
           <div>{{ book.category.name }}</div>
           <div>{{ book.category.name }}</div>
-          <div v-for="book of book.category.books" :key="book.id">
-            <router-link :to="`/book/${book.id}`">
-              {{ book.id }}. {{ book.title }}
-            </router-link>
-            <div>{{book.author}}</div>
-            <img :src="book.image" :alt="book.title">
+<!--          <div v-for="book of book.category.books" :key="book.id">-->
+<!--            <router-link :to="`/book/${book.id}`">-->
+<!--              {{ book.id }}. {{ book.title }}-->
+<!--            </router-link>-->
+<!--            <div>{{ book.author }}</div>-->
+<!--            <img :src="book.image" :alt="book.title">-->
+<!--          </div>-->
+<!--         -->
+          <div>
+            <router-link :to="`/book/${book.id}/edit`" class="link-margin">edit</router-link>
+            <a href="#" @click.prevent="deletedBook"  class="link-margin">delete</a>
           </div>
         </div>
       </template>
@@ -26,12 +30,25 @@
 
 <script>
 import queryBook from '@/graphql/queries/Book.gql'
+import deleteBook from '@/graphql/mutations/DeleteBook.gql'
 
 export default {
   name: "Book",
   data: () => ({
     query: queryBook,
-  })
+  }),
+  methods:{
+    deletedBook() {
+      this.$apollo.mutate({
+        mutation: deleteBook,
+        variables: {
+          id: this.$route.params.id,
+        }
+      }).then(()=>{
+        this.$router.push(`/`)
+      })
+}
+  }
 }
 </script>
 
